b4b93c02dd1e976bde1505e72b80d90fa79253eb
agrawal-priyanka <129191002+agrawal-priyanka@users.noreply.github.com>
2023-09-02 13:25:35 +0530

    Update Power Bi.py

--- pages/Power Bi.py
+++ pages/Power Bi.py
@@ -8,4 +8,4 @@ st.header("Power BI")
 
 st.write("Raw sales data for a grocery selling company, which deals in several product categories and subcategories, spans the years 2015 to 2018 (four years). We have been tasked with creating a dashboard with all the essential information about their historical sales success.")
 
-st.markdown('<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMGRlMGM1NWQtMWJmMC00YzJmLWFjNDEtZmRkMjUxNDkzY2FlIiwidCI6ImQwNjQ5MWQ4LTg5MjAtNDdmZS1iMmU4LTllMTY1ZTY3YzYxNCJ9" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
+st.markdown('<iframe title="Grocery Sales" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMGRlMGM1NWQtMWJmMC00YzJmLWFjNDEtZmRkMjUxNDkzY2FlIiwidCI6ImQwNjQ5MWQ4LTg5MjAtNDdmZS1iMmU4LTllMTY1ZTY3YzYxNCJ9" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
